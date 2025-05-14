49758a0bdaa1ac5cd59bb966dd008679fdd1a1e2
The-Yearly <theyearlysclub@gmail.com>
2025-05-14 13:59:59 +0530

    Changed Background of side menu

--- front-end/app/components/navBar.tsx
+++ front-end/app/components/navBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState,useEffect } from "react"
-import { User2Icon,Settings2 ,Menu, X } from "lucide-react"
+import { User2Icon,Settings2 ,Menu,FileText, X } from "lucide-react"
 import Link from "next/link"
 import { Session } from "@/middleware"
 import Cookies from "js-cookie"
@@ -20,9 +20,12 @@ export function Navbar() {
   
   return (
       <div className="flex fixed z-30 w-full backdrop-blur h-16 items-center justify-between">
-        <Link href={"/"} className="font-bold text-xl ml-5">
-          myResume
-        </Link>
+ <div className="flex items-center gap-2">
+            <div className="p-2 rounded-md">
+              <FileText className="h-6 w-6 text-sky-400" />
+            </div>
+            <Link href={"/"} className="font-bold text-xl">myResume</Link>
+          </div>
         <button className="md:hidden" onClick={toggleMenu}>
           {isMenuOpen ? <X className="mr-5 h-6 w-6" /> : <Menu className="mr-5 h-6 w-6" />}
           <div className="sr-only">Toggle menu</div>
@@ -41,7 +44,7 @@ export function Navbar() {
           </Link>
         </nav>
         {isMenuOpen && (
-          <div className="absolute top-16 left-0 right-0 bg-background border-b md:hidden">
+          <div className="absolute top-16 left-0 right-0 bg-gradient-to-br from-purple-900 to-black md:hidden">
             <div className="container py-4 flex flex-col gap-4">
               <Link
                 href={(logged.uid!=undefined&&logged.uid!=0)?`/resume/${logged.uid}`:"/authentication"}
